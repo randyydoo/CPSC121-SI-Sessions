980ab47fea5ffdcb579a6b35b3371724de02417a
randyydoo <randyydoo@gmail.com>
2024-01-24 21:22:31 -0800

    added practice files

---
+++ week1/jan25/main.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int multiply(int num1, int num2){
+    /* TODO */
+    /* write a function that computes the product and return the product */
+}
+
+double divide(double num1, double num2){
+    /* TODO */
+    /* write a function that computes the quotient and return the quotient */
+}
+
+double divide_or_multiply(){
+    /* 1. fill in parameters as necessary */
+    /* compute arithmitic based on instruction */
+ 
+}
+
+int main(){
+    int val1 = 326, val2 = 400;
+    unsigned int product;
+    /* TODO: call the multiply function and assign the value to the variable ~product~ */
+
+    cout << product << endl;
+
+    
+    
+    double val3 = 31, val4 = 16;
+    double quotient;
+    /* TODO: call the divide function and assign the value to the variable ~quotient~ */
+
+    cout << setprecision(4) << quotient << endl;
+    
+
+
+
+
+    /* CHALLENGE TODO: fill in divide_or_multiply function and compute following */
+    double challenge_num1 = 20.0, challenge_num2 = 12;
+    double challenge_product, challenge_quotient;
+    /* CHALLENGE TODO: call divide_or_multiply function and assign to variables ~challenge_product~ and ~challenge_quotient~ */
+
+
+    cout << challenge_product << endl << setprecision(4) << challenge_quotient;
+    
+    return 0;
+    
+
+}
